7097a80b71cfe7a2b13ca82fadb55cc847124e65
Simon <simonfong6@gmail.com>
2018-11-24 04:53:21 -0800

    Simple chrome file working

--- auto_apply/main.py
+++ auto_apply/main.py
@@ -1,3 +1,22 @@
 """
 Applies to companies.
 """
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
